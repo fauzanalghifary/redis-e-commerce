a62ce87ddb98281529450ea968b6f0c7933b11f3
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-05-05 20:13:40 +0700

    finish section 07

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,30 @@
 import type { Session } from '$services/types';
+import {sessionsKeys} from '$services/keys';
+import {client} from '$services/redis';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+	const session = await client.hGetAll(sessionsKeys(id))
+	if (Object.keys(session).length === 0) {
+		return null
+	}
+	return deserialize(id, session)
+};
 
-export const saveSession = async (session: Session) => {};
+export const saveSession = async (session: Session) => {
+	await client.hSet(sessionsKeys(session.id), serialize(session))
+};
+
+const serialize = (session: Session) => {
+	return {
+		userId: session.userId,
+		username: session.username
+	}
+}
+
+const deserialize = (id: string, session: {[key: string]: string}) => {
+	return {
+		id: id,
+		userId: session.userId,
+		username: session.username
+	}
+}
