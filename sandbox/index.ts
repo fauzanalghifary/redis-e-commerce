c84577a5e54c10d0e8b70a37e9228305bb6944a9
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-05-05 20:25:44 +0700

    finish section 08

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,17 +2,28 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: '2020',
-		// engine: {
-		// 	cylinder: 8
-		// },
-		// owner: null || '',
-		// service: undefined || ''
 	})
 
-	const car = await client.hGetAll('car');
-	console.log(car);
+	await client.hSet('car1', {
+		color: 'yellow',
+		year: '2030',
+	})
+
+	await client.hSet('car1', {
+		color: 'green',
+		year: '2040',
+	})
+
+	const commands = [1, 2, 3].map((id) => {
+		return client.hGetAll('car' + id);
+	});
+
+	const results = await Promise.all(commands)
+
+	console.log(results);
+
 };
 run();
