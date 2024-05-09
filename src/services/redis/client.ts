71e0bd74fdb479df4c7c26a1dd8006d2473e44c2
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-05-09 23:00:00 +0700

    finish section 19

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -48,6 +48,22 @@ const client = createClient({
 			},
 			transformReply() {
 			}
+		}),
+		unlock: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT: `
+				if redis.call('GET', KEYS[1]) == ARGV[1] then
+					return redis.call('DEL', KEYS[1])
+				else
+					return 0
+				end
+			`,
+			transformArguments(key: string, token: string) {
+				return [key, token];
+			},
+			transformReply(reply: any) {
+				return reply;
+			}
 		})
 	}
 });
